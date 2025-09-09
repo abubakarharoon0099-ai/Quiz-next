8d84de797ce282e3b16113675205ea4d22bcdfe5
Abubakarharoon <adanhussain111@gmail.com>
2025-09-09 16:03:59 +0500

    load time error

--- src/components/ResultScreen.tsx
+++ src/components/ResultScreen.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 export default function ResultScreen({ score, total }: { score: number; total: number }) {
   const router = useRouter()
@@ -16,12 +17,13 @@ export default function ResultScreen({ score, total }: { score: number; total: n
         <p className="text-xl text-red-500">Sorry! You Failed. Better luck next time 😊</p>
       )}
       <div className="flex flex-col items-center gap-8">
-        <button
-          onClick={() => router.push("/")}
-          className="text-xl font-[400] px-4 py-2 bg-[#dcdedc] border border-black rounded-md hover:bg-[#9f9f9f] transition-colors cursor-pointer w-[200px]"
-        >
-          Go to Home
-        </button>
+       <Link
+  href="/"
+  className="text-xl font-[400] px-4 py-2 bg-[#dcdedc] border border-black rounded-md hover:bg-[#9f9f9f] transition-colors cursor-pointer w-[200px] text-center"
+>
+  Go to Home
+</Link>
+
       </div>
     </div>
   )
